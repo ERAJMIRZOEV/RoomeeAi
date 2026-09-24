0d648b822b1689d05ab43b6166360509e28730f2
Eraj Mirzoev <mirzoeva145@gmail.com>
2026-09-24 16:55:02 +0500

    fix(axios): add fallback URL to prevent ERR_INVALID_URL on build

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,9 @@
+// src/lib/axios.ts
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: process.env.NEXt_PUBLIC_MOKKY_API_URL,
-    headers: {
-        'Content-Type': 'application/json'
-    },
-})
+  baseURL: process.env.NEXT_PUBLIC_MOKKY_API_URL || 'https://d20c118218070076.mokky.dev',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
